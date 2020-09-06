7e2e6cb2258f2c96dc061cffa5ec27063b76a194
mayunmeiyouming <huangw@mvalley.io>
2020-09-06 15:28:25 +0800

    fix

--- cmd/panda/server.go
+++ cmd/panda/server.go
@@ -1,18 +1,75 @@
 package panda
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
-	"Panda/internal/panda"
+	"net"
+	"net/url"
+	"strings"
 )
 
 // Server 是 Panda 的实际入口
 func Server(listen string, socks string) {
-	log.Println(listen)
-	log.Println(socks)
-
-	if listen != "" {
-		log.Println("Starting to listen for clients")
-		go panda.ListenForSocks(listen)
-		log.Fatal(panda.ListenForClients(socks))
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
 	}
+	log.Println("等待连接")
+	for {
+		client, err := l.Accept()
+		log.Println("收到请求")
+		if err != nil {
+			log.Panic(err)
+		}
+		go handleClientRequest(client)
+	}
+}
+
+func handleClientRequest(client net.Conn) {
+	if client == nil {
+		return
+	}
+	defer client.Close()
+	var b [1024]byte
+	n, err := client.Read(b[:])
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	var method, host, address string
+	fmt.Sscanf(string(b[:bytes.IndexByte(b[:], '\n')]), "%s%s", &method, &host)
+	hostPortURL, err := url.Parse(host)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if hostPortURL.Opaque == "443" { //https访问
+		address = hostPortURL.Scheme + ":443"
+	} else {
+		//http访问
+		if strings.Index(hostPortURL.Host, ":") == -1 { //host不带端口， 默认80
+			address = hostPortURL.Host + ":80"
+		} else {
+			address = hostPortURL.Host
+		}
+	}
+
+	//获得了请求的host和port，就开始拨号吧
+	server, err := net.Dial("tcp", address)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if method == "CONNECT" {
+		fmt.Fprint(client, "HTTP/1.1 200 Connection established\r\n")
+	} else {
+		server.Write(b[:n])
+	}
+
+	//进行转发
+	go io.Copy(server, client)
+	io.Copy(client, server)
 }
