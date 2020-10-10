a17dd694061e47d3a4744e9acdbe4014cd06f5d8
mayunmeiyouming <2510764133@qq.com>
2020-10-11 01:06:11 +0800

    增加AEAD_XCHACHA20_POLY1305

--- pkg/core/cipher.go
+++ pkg/core/cipher.go
@@ -29,10 +29,11 @@ type PacketConnCipher interface {
 var ErrCipherNotSupported = errors.New("cipher not supported")
 
 const (
-	aeadAes128Gcm        = "AEAD_AES_128_GCM"
-	aeadAes192Gcm        = "AEAD_AES_192_GCM"
-	aeadAes256Gcm        = "AEAD_AES_256_GCM"
-	aeadChacha20Poly1305 = "AEAD_CHACHA20_POLY1305"
+	aeadAes128Gcm         = "AEAD_AES_128_GCM"
+	aeadAes192Gcm         = "AEAD_AES_192_GCM"
+	aeadAes256Gcm         = "AEAD_AES_256_GCM"
+	aeadChacha20Poly1305  = "AEAD_CHACHA20_POLY1305"
+	aeadXChacha20Poly1305 = "AEAD_XCHACHA20_POLY1305"
 )
 
 // List of AEAD ciphers: key size in bytes and constructor
@@ -40,10 +41,11 @@ var aeadList = map[string]struct {
 	KeySize int
 	New     func([]byte) (shadowaead.Cipher, error)
 }{
-	aeadAes128Gcm: {16, shadowaead.AESGCM},
-	aeadAes192Gcm: {24, shadowaead.AESGCM},
-	aeadAes256Gcm: {32, shadowaead.AESGCM},
-	aeadChacha20Poly1305: {32, shadowaead.Chacha20Poly1305},
+	aeadAes128Gcm:         {16, shadowaead.AESGCM},
+	aeadAes192Gcm:         {24, shadowaead.AESGCM},
+	aeadAes256Gcm:         {32, shadowaead.AESGCM},
+	aeadChacha20Poly1305:  {32, shadowaead.Chacha20Poly1305},
+	aeadXChacha20Poly1305: {32, shadowaead.XChacha20Poly1305},
 }
 
 // ListCipher returns a list of available cipher names sorted alphabetically.
@@ -63,6 +65,8 @@ func PickCipher(name string, password string) (CipherConn, error) {
 	switch name {
 	case "DUMMY":
 		return &dummy{}, nil
+	case "XCHACHA20-IETF-POLY1305":
+		name = aeadXChacha20Poly1305
 	case "CHACHA20-IETF-POLY1305":
 		name = aeadChacha20Poly1305
 	case "AES-128-GCM":
